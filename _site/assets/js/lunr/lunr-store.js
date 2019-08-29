0795c888a87ac245832ff2c5e6020e7933e6c382
indexxlim <indexxlim@gmail.com>
2019-08-29 14:42:09 +0900

    'git-io-commit'

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/first-post/",
+        "teaser":null},{
+        "title": "Git Upload",
+        "excerpt":"$ git add . Adds the files in the local repository and stages them for commit. To unstage a file, use ‘git reset HEAD YOUR-FILE’. $ git commit -m \"First commit\" Commits the tracked changes and prepares them to be pushed to a remote repository. To remove this commit and...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/git-upload/",
+        "teaser":null}]
