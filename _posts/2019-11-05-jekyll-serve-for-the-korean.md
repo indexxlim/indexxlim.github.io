e9c1eadf07398e08855a923c14e193daece964eb
indexxlim <indexxlim@gmail.com>
2019-11-05 15:44:55 +0900

    'add_graph'

---
+++ _posts/2019-11-05-jekyll-serve-for-the-korean.md
@@ -0,0 +1,12 @@
+---
+layout: single
+author_profile: true
+read_time: true
+comments: true
+share: true
+related: true
+title: jekyll serve for the Korean
+---
+
+chcp 65001
+bundle exec jekyll serve
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"$ git add . Adds the files in the local repository and stages them for commit. To unstage a file, use ‘git reset HEAD YOUR-FILE’. $ git commit -m \"First commit\" Commits the tracked changes and prepares them to be pushed to a remote repository. To remove this commit and...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/git-upload/",
+        "teaser":null},{
+        "title": "jekyll serve for the Korean",
+        "excerpt":"chcp 65001 bundle exec jekyll serve  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll-serve-for-the-korean/",
         "teaser":null}]
