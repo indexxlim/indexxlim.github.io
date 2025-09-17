919fa456f899555311280aec08c9958b7f9a0f4e
indexxlim <indexxlim@gmail.com>
2025-09-17 10:18:46 +0900

    feat: action:

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://indexxlim@github.io',
+  url: 'https://indexxlim.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -29,6 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'indexxlim', // Usually your GitHub org/user name.
   projectName: 'indexxlim.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
